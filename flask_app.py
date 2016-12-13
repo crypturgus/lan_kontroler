5d831c61e1318176bbc681127717fa2838e9e85e
mati <mkukawski@gmial.com>
2016-12-13 22:38:55 +0100

    test get from remote server

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,65 @@
-# A very simple Flask Hello World app for you to get started with...
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
 
-from flask import Flask
-
+# create our little application :)
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'lancontrol.db'),
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='admin'
+))
+app.config.from_envvar('FLASK_APP_SETTINGS', silent=True)
+# boardchart
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print 'Initialized the database.'
+
+
+
 
 @app.route('/')
 def hello_world():
     return 'Hello in LanControl App!'
+from flask import request
+
+@app.route('/db', methods=['GET'])
+def login():
+    if request.method == 'GET':
+        with open('dupa.txt', 'w') as f:
+            f.write('dupa')
